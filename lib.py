8ce9594a6c525018f19a23defa24c6ca70086b1f
ひびき <d3npa@pm.me>
2019-07-31 12:25:49 +0900

    Added header parsing

--- lib.py
+++ lib.py
@@ -9,6 +9,12 @@ import re
 control_chars = "".join(map(chr, list(range(0,32)) + list(range(127,160))))
 re_no_cc = re.compile("[%s]" % re.escape(control_chars))
 
+keywords = {
+        "HEADER_HEAD" : "START_HEADERS",
+        "HEADER_TAIL" : "END_HEADERS"
+}
+
+
 def get_ip():
         # クライアントのIPアドレスを返す。
         return request.headers["X-Forwarded-For"] if "X-Forwarded-For" in request.headers else request.remote_addr
@@ -33,3 +39,29 @@ def read_file(path):
         with open(path, "rb") as f:
                 return f.read()
 
+def read_post(path):
+        # ファイルパスから記事をパース、ヘッダーを辞書で返し内容をMarkdownとして返す。
+        # 不正なパスの場合エラー吹きますので呼び出す前にチェックしましょう。
+        headers = {}
+        contents = ""
+        with open(path, "r") as f:
+                parse_headers = False
+                line_index = 0
+                data = [line[:-1] for line in f.readlines()]
+                if data[line_index] == keywords["HEADER_HEAD"]:
+                        parse_headers = True
+                while parse_headers:
+                        line_index += 1
+                        if line:
+                                line = data[line_index]
+                                if line == keywords["HEADER_TAIL"]:
+                                        parse_headers = False
+                                        line_index += 1
+                                        break
+                                line = line.split(": ")
+                                headers.update({line[0] : line[1]})
+                contents = "\n".join(data[line_index:])
+        return {
+                "headers" : headers,
+                "contents" : contents
+        }
